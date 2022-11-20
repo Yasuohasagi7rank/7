3ca78ac102f618cfaad4573f77651e861c398a9e
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:01:15 +0300

    Add files via upload

---
+++ 4.52/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _4._52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, b, c;
+            Console.WriteLine("Длина: ");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ширина: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Диаметр: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if ((c + 2) < a && (c + 2) < b)
+            {
+                Console.WriteLine("Вася сможет");
+            } 
+            else
+            {
+                Console.WriteLine("Вася не сможет");
+            }
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
